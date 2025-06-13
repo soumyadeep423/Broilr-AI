0f36ba0b9de9496141aaee39d684ddc33fb938a8
soumyadeep423 <soumyadeep.das423@gmail.com>
2025-06-13 21:54:22 +0530

    updated app.py

--- app.py
+++ app.py
@@ -121,4 +121,5 @@ def delete_recipe():
 # ------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT dynamically
+    app.run(host="0.0.0.0", port=port)
